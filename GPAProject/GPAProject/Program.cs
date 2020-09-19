32f6557e16146f9294e5100c6dd24df599ad1824
Makiolix <71552425+Makiolix@users.noreply.github.com>
2020-09-19 13:51:44 +0200

    Add files via upload

---
+++ GPAProject/GPAProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntroductionToCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the student's first name.");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter the student's surname.");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter the student's GPA.");
+            string GPA = Console.ReadLine();
+
+            Console.WriteLine("Student {0} {1}, GPA {2}", FirstName, LastName, GPA);
+
+        }
+    }
+}
