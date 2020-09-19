62e654f818dacb462233cdd391b5af8efdc6664d
Makiolix <71552425+Makiolix@users.noreply.github.com>
2020-09-19 13:41:44 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number");
+
+            int UserNumber = int.Parse(Console.ReadLine());
+
+            if (UserNumber == 1)
+            {
+                Console.WriteLine("Your number is one");
+            }
+
+            if (UserNumber == 2)
+            {
+                Console.WriteLine("Your number is two");
+            }
+
+            if (UserNumber == 3)
+            {
+                Console.WriteLine("Your number is three");
+            }
+
+            else
+            {
+                Console.WriteLine("Your number is not between 1 and 3");
+            }
+
+        }
+    }
+}
